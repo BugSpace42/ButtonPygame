2c198fa688bcf603149bc6d70ea2a122d08cecc9
BugSpace <409478@niuitmo.ru>
2026-01-31 16:58:22 +0700

    add label

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ font = pygame.font.Font(None, 48)
 label_font = pygame.font.Font(None, 36)
 
 # Кнопка + 1
-plus_button = pygame.Rect(screen_width//8, screen_width*3//4, screen_width//4, screen_width//8)
+plus_button = pygame.Rect(screen_width//8, screen_width*5//8, screen_width//4, screen_width//8)
 # Кнопка - 1
-minus_button = pygame.Rect(screen_width*5//8, screen_width*3//4, screen_width//4, screen_width//8)
+minus_button = pygame.Rect(screen_width*5//8, screen_width*5//8, screen_width//4, screen_width//8)
 
 clock = pygame.time.Clock()
 running = True
@@ -31,6 +31,14 @@ while running:
     minus_color = (200, 0, 0)
     pygame.draw.rect(screen, minus_color, minus_button)
     pygame.draw.rect(screen, (0, 0, 0), minus_button, 2)  # Обводка
+
+    label_text = label_font.render("Текущее значение:", True, (255, 255, 255))
+    label_rect = label_text.get_rect(center=(screen_width // 2, screen_width // 8))
+    screen.blit(label_text, label_rect)
+
+    counter_text = font.render(str(counter), True, (0, 0, 200))
+    text_rect = counter_text.get_rect(center=(screen_width // 2, screen_width* 3 // 8))
+    screen.blit(counter_text, text_rect)
 
     pygame.display.flip()
 
